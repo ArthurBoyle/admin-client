759468b5a75302a670bd7d4ff6ff02044ab606d2
ArthurBoyle <75610914+ArthurBoyle@users.noreply.github.com>
2021-03-06 14:33:40 +0800

    完成Header组件与退出登录

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,28 @@
 import ajax from "./ajax";
+import jsonp from "jsonp";
+import {message} from "antd";
 
 // 1.登录
 export const reqLogin = (username, password) => ajax("/login", "POST", {username, password});
+
+// 2. jsonp请求的天气接口请求函数
+export const reqWeather = () => {
+    return new Promise((resolve) => {
+        const ip = "https://restapi.amap.com/v3/ip?output=json&key=2727727a44affdf8682c93d963e7984f";
+        jsonp(ip, {}, (error, data) => {
+            if (!error && data.status === "1") {
+                const weather = `https://restapi.amap.com/v3/weather/weatherInfo?city=${data.adcode}&key=2727727a44affdf8682c93d963e7984f`;
+                jsonp(weather, {}, (error, data) => {
+                    if (!error && data.status === "1") {
+                        const {weather, temperature} = data.lives[0];
+                        resolve({weather, temperature});
+                    } else {
+                        message.error("获取天气信息失败");
+                    }
+                });
+            } else {
+                message.error("获取IP地址失败");
+            }
+        });
+    });
+}
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,89 @@
 import React, {Component} from 'react';
+import {withRouter} from "react-router-dom";
+import { Modal } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+import {reqWeather} from "../../api";
+import {formateDate} from "../../utils/dateUtils";
+import menuList from "../../config/menuConfig";
+import memoryUtils from "../../utils/memoryUtils";
+import storageUtils from "../../utils/storageUtils";
+import LinkButton from "../link-button";
 import "./index.less";
 
-export default class Header extends Component {
+const { confirm } = Modal;
+
+class Header extends Component {
+    state = {
+        currentTime: formateDate(Date.now()),
+        weather: "",
+        temperature: ""
+    };
+    async componentDidMount() {
+        this.timer = setInterval(() => {
+            const currentTime = formateDate(Date.now());
+            this.setState({currentTime});
+        }, 1000);
+        const {weather, temperature} = await reqWeather();
+        this.setState({weather, temperature});
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    getTitle = () => {
+        const path = this.props.location.pathname;
+        let title;
+        menuList.forEach((item) => {
+            if (item.key === path) {
+                title = item.title;
+            } else if (item.children) {
+                // item.children.forEach((cItem) => {
+                //     if (cItem.key === path) {
+                //         title = cItem.title;
+                //     }
+                // })
+                const cItem = item.children.find((cItem) => cItem.key === path);
+                if (cItem) {
+                    title = cItem.title;
+                }
+            }
+        });
+        return title;
+    }
+
+    logout = () => {
+        confirm({
+            title: 'Do you Want to logout current user?',
+            icon: <ExclamationCircleOutlined />,
+            onOk:() => {
+                memoryUtils.user = {};
+                storageUtils.removeUser();
+                this.props.history.replace("/login");
+            }
+        });
+    }
+
     render() {
+        const {currentTime, weather, temperature} = this.state;
+        const {username} = memoryUtils.user;
         return (
             <div className="header">
-                Header
+                <div className="header-top">
+                    <span>欢迎，{username}</span>
+                    <LinkButton onClick={this.logout}>退出</LinkButton>
+                </div>
+                <div className="header-bottom">
+                    <div className="header-bottom-left">{this.getTitle()}</div>
+                    <div className="header-bottom-right">
+                        <span>{currentTime}</span>
+                        <span>{weather}</span>
+                        <span>{temperature}°C</span>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+export default withRouter(Header);
